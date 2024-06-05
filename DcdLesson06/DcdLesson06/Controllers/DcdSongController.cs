f24feabf296821de358cfe45c3e532ee7cdb7a24
Dinhmobile35 <136127701+Dinhmobile35@users.noreply.github.com>
2024-06-05 12:12:19 +0700

    lesson06

---
+++ DcdLesson06/DcdLesson06/Controllers/DcdSongController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DcdLesson06.Models;
+
+namespace DcdLesson06.Controllers
+{
+    public class DcdSongController : Controller
+    {
+        private static List<DcdSong> dcdSongs = new List<DcdSong>() 
+        { 
+            new DcdSong{Id=221090095,DcdTitle="Đinh Công Định",DcdAuthor="K22CNT4",DcdArtist="NTU",DcdYearRelease=2022 },
+             new DcdSong{Id=221090096,DcdTitle="Đinh Công Định",DcdAuthor="K22CNT4",DcdArtist="NTU",DcdYearRelease=2022 }
+        };
+        // GET: DcdSong
+        /// <summary>
+        /// Hiển thị danh sách bài hát
+        /// Author :Đinh Công Định 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DcdIndex()
+        {
+            return View(dcdSongs);
+        }
+        //Get : DcdCreate 
+        /// <summary>
+        /// Thêm mới bài hát
+        /// Author :Đinh Công Định 
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult DcdCreate()
+        {
+            var dcdSong = new DcdSong();
+            return View(dcdSong);
+        }
+    }
+}
---
+++ DcdLesson06/DcdLesson06/Models/DcdSong.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DcdLesson06.Models
+{
+    public class DcdSong
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required (ErrorMessage ="Dcd: hãy nhập tiêu đề")]
+        [DisplayName("tiêu đề")]
+        public string DcdTitle { get; set; }
+        [Required (ErrorMessage ="Dcd: hãy nhập tác giả ")]
+        [DisplayName("tác giả")]
+        public string DcdAuthor { get; set; }
+        [Required (ErrorMessage ="Dcd: hãy nhập nghệ sĩ ")]
+        [StringLength (50,MinimumLength =2,ErrorMessage ="Dcd: tên nghệ sĩ tối thiêu 2 ký tự,tối đa 50 ")]
+        [DisplayName("nghệ sĩ" )]
+        public string DcdArtist { get; set; }
+        [Required (ErrorMessage ="Dcd: Hãy nhập năm xuất bản")]
+        [RegularExpression(@"[0-9]{4} ",ErrorMessage ="Dcd:Năm xuất bản phải có 4 ký tự số") ]
+        [Range(1900,2024,ErrorMessage ="Dcd: Năm xuất bản trong khoảng 1900-2024")]
+        [DisplayName("năm xuất bản")]
+        public int DcdYearRelease { get; set; }
+    }
+}
